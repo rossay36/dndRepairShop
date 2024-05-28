806a8428770459baff6c89c24827e441ef59e595
Obi Ross <obi75309@gmail.com>
2024-05-28 16:42:30 +0100

    updated baseQury url

--- src/features/app/api/apiSlice.js
+++ src/features/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://rossay.onrender.com",
+  baseUrl: "https://rossays.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
